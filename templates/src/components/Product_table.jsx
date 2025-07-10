908c9f11181e9f31d84d60735b6a16af496f03ab
Shubham Jha <jhashubham059@gmail.com>
2025-07-11 01:52:38 +0530

    searchbar and table implemented

---
+++ templates/src/components/Product_table.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { data } from '../walmart_products';
+
+function ProductTable(){
+    const [search, setSearch] = useState('');
+
+
+    return (
+        <div className='product-table'>
+
+
+            <div className='search-bar'>
+                <input type="text" name='search' placeholder='Search Product' value={search} onChange={(e)=> setSearch(e.target.value)} />
+            </div>
+
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Category</th>
+                        <th>In Stoke</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.filter((product) => {
+                        return search.toLowerCase() === '' ? product :
+                        product.name.toLowerCase().includes(search);
+                    }).map((product) => (
+                        <tr key={product.id}>
+                        <td>{product.id}</td>
+                        <td>{product.name}</td>
+                        <td>{product.category}</td>
+                        <td>{product.in_stock}</td>
+                        <td>{product.status}</td>
+                        </tr>
+                    ))}
+                    
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default ProductTable;
---
+++ templates/src/walmart_products.js
@@ -0,0 +1,702 @@
+export const data = [
+    {
+        "id": 1,
+        "name": "Mainstays Bed Sheet Set #1",
+        "category": "Clothing",
+        "in_stock": 110,
+        "status": "inactive"
+    },
+    {
+        "id": 2,
+        "name": "Great Value Peanut Butter #2",
+        "category": "Beauty",
+        "in_stock": 57,
+        "status": "active"
+    },
+    {
+        "id": 3,
+        "name": "Equate Ibuprofen #3",
+        "category": "Sports",
+        "in_stock": 273,
+        "status": "active"
+    },
+    {
+        "id": 4,
+        "name": "Ozark Trail Water Bottle #4",
+        "category": "Sports",
+        "in_stock": 56,
+        "status": "active"
+    },
+    {
+        "id": 5,
+        "name": "Parent's Choice Diapers #5",
+        "category": "Electronics",
+        "in_stock": 440,
+        "status": "active"
+    },
+    {
+        "id": 6,
+        "name": "Hyper Tough Drill #6",
+        "category": "Beauty",
+        "in_stock": 268,
+        "status": "discontinued"
+    },
+    {
+        "id": 7,
+        "name": "Better Homes & Gardens Candle #7",
+        "category": "Beauty",
+        "in_stock": 421,
+        "status": "inactive"
+    },
+    {
+        "id": 8,
+        "name": "Blackweb Wireless Mouse #8",
+        "category": "Toys",
+        "in_stock": 185,
+        "status": "active"
+    },
+    {
+        "id": 9,
+        "name": "Onn. HDMI Cable #9",
+        "category": "Home & Kitchen",
+        "in_stock": 320,
+        "status": "inactive"
+    },
+    {
+        "id": 10,
+        "name": "George Men's T-Shirt #10",
+        "category": "Books",
+        "in_stock": 370,
+        "status": "inactive"
+    },
+    {
+        "id": 11,
+        "name": "Time and Tru Women's Jeans #11",
+        "category": "Books",
+        "in_stock": 286,
+        "status": "discontinued"
+    },
+    {
+        "id": 12,
+        "name": "No Boundaries Hoodie #12",
+        "category": "Electronics",
+        "in_stock": 479,
+        "status": "discontinued"
+    },
+    {
+        "id": 13,
+        "name": "Your Zone Kids' Comforter #13",
+        "category": "Home & Kitchen",
+        "in_stock": 419,
+        "status": "discontinued"
+    },
+    {
+        "id": 14,
+        "name": "Hart 20V Battery #14",
+        "category": "Clothing",
+        "in_stock": 276,
+        "status": "active"
+    },
+    {
+        "id": 15,
+        "name": "Pen+Gear Notebook #15",
+        "category": "Home & Kitchen",
+        "in_stock": 360,
+        "status": "inactive"
+    },
+    {
+        "id": 16,
+        "name": "Sam's Choice Coffee #16",
+        "category": "Books",
+        "in_stock": 281,
+        "status": "active"
+    },
+    {
+        "id": 17,
+        "name": "Marketside Fresh Salad #17",
+        "category": "Home & Kitchen",
+        "in_stock": 225,
+        "status": "inactive"
+    },
+    {
+        "id": 18,
+        "name": "Wonder Nation Kids' Shoes #18",
+        "category": "Sports",
+        "in_stock": 193,
+        "status": "discontinued"
+    },
+    {
+        "id": 19,
+        "name": "Protege Suitcase #19",
+        "category": "Electronics",
+        "in_stock": 194,
+        "status": "active"
+    },
+    {
+        "id": 20,
+        "name": "Athletic Works Joggers #20",
+        "category": "Beauty",
+        "in_stock": 159,
+        "status": "inactive"
+    },
+    {
+        "id": 21,
+        "name": "Mainstays Bed Sheet Set #21",
+        "category": "Sports",
+        "in_stock": 173,
+        "status": "inactive"
+    },
+    {
+        "id": 22,
+        "name": "Great Value Peanut Butter #22",
+        "category": "Toys",
+        "in_stock": 193,
+        "status": "inactive"
+    },
+    {
+        "id": 23,
+        "name": "Equate Ibuprofen #23",
+        "category": "Sports",
+        "in_stock": 461,
+        "status": "active"
+    },
+    {
+        "id": 24,
+        "name": "Ozark Trail Water Bottle #24",
+        "category": "Books",
+        "in_stock": 339,
+        "status": "discontinued"
+    },
+    {
+        "id": 25,
+        "name": "Parent's Choice Diapers #25",
+        "category": "Toys",
+        "in_stock": 347,
+        "status": "inactive"
+    },
+    {
+        "id": 26,
+        "name": "Hyper Tough Drill #26",
+        "category": "Electronics",
+        "in_stock": 113,
+        "status": "active"
+    },
+    {
+        "id": 27,
+        "name": "Better Homes & Gardens Candle #27",
+        "category": "Sports",
+        "in_stock": 81,
+        "status": "inactive"
+    },
+    {
+        "id": 28,
+        "name": "Blackweb Wireless Mouse #28",
+        "category": "Electronics",
+        "in_stock": 421,
+        "status": "discontinued"
+    },
+    {
+        "id": 29,
+        "name": "Onn. HDMI Cable #29",
+        "category": "Sports",
+        "in_stock": 146,
+        "status": "discontinued"
+    },
+    {
+        "id": 30,
+        "name": "George Men's T-Shirt #30",
+        "category": "Clothing",
+        "in_stock": 273,
+        "status": "discontinued"
+    },
+    {
+        "id": 31,
+        "name": "Time and Tru Women's Jeans #31",
+        "category": "Electronics",
+        "in_stock": 190,
+        "status": "discontinued"
+    },
+    {
+        "id": 32,
+        "name": "No Boundaries Hoodie #32",
+        "category": "Books",
+        "in_stock": 2,
+        "status": "discontinued"
+    },
+    {
+        "id": 33,
+        "name": "Your Zone Kids' Comforter #33",
+        "category": "Home & Kitchen",
+        "in_stock": 40,
+        "status": "inactive"
+    },
+    {
+        "id": 34,
+        "name": "Hart 20V Battery #34",
+        "category": "Electronics",
+        "in_stock": 85,
+        "status": "inactive"
+    },
+    {
+        "id": 35,
+        "name": "Pen+Gear Notebook #35",
+        "category": "Toys",
+        "in_stock": 91,
+        "status": "inactive"
+    },
+    {
+        "id": 36,
+        "name": "Sam's Choice Coffee #36",
+        "category": "Clothing",
+        "in_stock": 407,
+        "status": "active"
+    },
+    {
+        "id": 37,
+        "name": "Marketside Fresh Salad #37",
+        "category": "Clothing",
+        "in_stock": 154,
+        "status": "discontinued"
+    },
+    {
+        "id": 38,
+        "name": "Wonder Nation Kids' Shoes #38",
+        "category": "Sports",
+        "in_stock": 440,
+        "status": "discontinued"
+    },
+    {
+        "id": 39,
+        "name": "Protege Suitcase #39",
+        "category": "Home & Kitchen",
+        "in_stock": 491,
+        "status": "discontinued"
+    },
+    {
+        "id": 40,
+        "name": "Athletic Works Joggers #40",
+        "category": "Beauty",
+        "in_stock": 21,
+        "status": "active"
+    },
+    {
+        "id": 41,
+        "name": "Mainstays Bed Sheet Set #41",
+        "category": "Clothing",
+        "in_stock": 303,
+        "status": "inactive"
+    },
+    {
+        "id": 42,
+        "name": "Great Value Peanut Butter #42",
+        "category": "Books",
+        "in_stock": 143,
+        "status": "inactive"
+    },
+    {
+        "id": 43,
+        "name": "Equate Ibuprofen #43",
+        "category": "Toys",
+        "in_stock": 271,
+        "status": "inactive"
+    },
+    {
+        "id": 44,
+        "name": "Ozark Trail Water Bottle #44",
+        "category": "Beauty",
+        "in_stock": 368,
+        "status": "inactive"
+    },
+    {
+        "id": 45,
+        "name": "Parent's Choice Diapers #45",
+        "category": "Electronics",
+        "in_stock": 83,
+        "status": "inactive"
+    },
+    {
+        "id": 46,
+        "name": "Hyper Tough Drill #46",
+        "category": "Beauty",
+        "in_stock": 129,
+        "status": "discontinued"
+    },
+    {
+        "id": 47,
+        "name": "Better Homes & Gardens Candle #47",
+        "category": "Electronics",
+        "in_stock": 128,
+        "status": "discontinued"
+    },
+    {
+        "id": 48,
+        "name": "Blackweb Wireless Mouse #48",
+        "category": "Clothing",
+        "in_stock": 171,
+        "status": "active"
+    },
+    {
+        "id": 49,
+        "name": "Onn. HDMI Cable #49",
+        "category": "Electronics",
+        "in_stock": 186,
+        "status": "inactive"
+    },
+    {
+        "id": 50,
+        "name": "George Men's T-Shirt #50",
+        "category": "Clothing",
+        "in_stock": 438,
+        "status": "inactive"
+    },
+    {
+        "id": 51,
+        "name": "Time and Tru Women's Jeans #51",
+        "category": "Sports",
+        "in_stock": 176,
+        "status": "inactive"
+    },
+    {
+        "id": 52,
+        "name": "No Boundaries Hoodie #52",
+        "category": "Electronics",
+        "in_stock": 100,
+        "status": "inactive"
+    },
+    {
+        "id": 53,
+        "name": "Your Zone Kids' Comforter #53",
+        "category": "Books",
+        "in_stock": 329,
+        "status": "discontinued"
+    },
+    {
+        "id": 54,
+        "name": "Hart 20V Battery #54",
+        "category": "Books",
+        "in_stock": 195,
+        "status": "active"
+    },
+    {
+        "id": 55,
+        "name": "Pen+Gear Notebook #55",
+        "category": "Sports",
+        "in_stock": 422,
+        "status": "discontinued"
+    },
+    {
+        "id": 56,
+        "name": "Sam's Choice Coffee #56",
+        "category": "Books",
+        "in_stock": 36,
+        "status": "active"
+    },
+    {
+        "id": 57,
+        "name": "Marketside Fresh Salad #57",
+        "category": "Beauty",
+        "in_stock": 422,
+        "status": "discontinued"
+    },
+    {
+        "id": 58,
+        "name": "Wonder Nation Kids' Shoes #58",
+        "category": "Home & Kitchen",
+        "in_stock": 291,
+        "status": "discontinued"
+    },
+    {
+        "id": 59,
+        "name": "Protege Suitcase #59",
+        "category": "Clothing",
+        "in_stock": 299,
+        "status": "inactive"
+    },
+    {
+        "id": 60,
+        "name": "Athletic Works Joggers #60",
+        "category": "Books",
+        "in_stock": 373,
+        "status": "discontinued"
+    },
+    {
+        "id": 61,
+        "name": "Mainstays Bed Sheet Set #61",
+        "category": "Beauty",
+        "in_stock": 182,
+        "status": "inactive"
+    },
+    {
+        "id": 62,
+        "name": "Great Value Peanut Butter #62",
+        "category": "Electronics",
+        "in_stock": 273,
+        "status": "discontinued"
+    },
+    {
+        "id": 63,
+        "name": "Equate Ibuprofen #63",
+        "category": "Beauty",
+        "in_stock": 412,
+        "status": "inactive"
+    },
+    {
+        "id": 64,
+        "name": "Ozark Trail Water Bottle #64",
+        "category": "Electronics",
+        "in_stock": 356,
+        "status": "active"
+    },
+    {
+        "id": 65,
+        "name": "Parent's Choice Diapers #65",
+        "category": "Toys",
+        "in_stock": 65,
+        "status": "active"
+    },
+    {
+        "id": 66,
+        "name": "Hyper Tough Drill #66",
+        "category": "Toys",
+        "in_stock": 395,
+        "status": "inactive"
+    },
+    {
+        "id": 67,
+        "name": "Better Homes & Gardens Candle #67",
+        "category": "Home & Kitchen",
+        "in_stock": 163,
+        "status": "discontinued"
+    },
+    {
+        "id": 68,
+        "name": "Blackweb Wireless Mouse #68",
+        "category": "Sports",
+        "in_stock": 137,
+        "status": "active"
+    },
+    {
+        "id": 69,
+        "name": "Onn. HDMI Cable #69",
+        "category": "Electronics",
+        "in_stock": 221,
+        "status": "inactive"
+    },
+    {
+        "id": 70,
+        "name": "George Men's T-Shirt #70",
+        "category": "Electronics",
+        "in_stock": 52,
+        "status": "inactive"
+    },
+    {
+        "id": 71,
+        "name": "Time and Tru Women's Jeans #71",
+        "category": "Home & Kitchen",
+        "in_stock": 419,
+        "status": "discontinued"
+    },
+    {
+        "id": 72,
+        "name": "No Boundaries Hoodie #72",
+        "category": "Sports",
+        "in_stock": 461,
+        "status": "active"
+    },
+    {
+        "id": 73,
+        "name": "Your Zone Kids' Comforter #73",
+        "category": "Clothing",
+        "in_stock": 101,
+        "status": "discontinued"
+    },
+    {
+        "id": 74,
+        "name": "Hart 20V Battery #74",
+        "category": "Clothing",
+        "in_stock": 332,
+        "status": "active"
+    },
+    {
+        "id": 75,
+        "name": "Pen+Gear Notebook #75",
+        "category": "Clothing",
+        "in_stock": 225,
+        "status": "active"
+    },
+    {
+        "id": 76,
+        "name": "Sam's Choice Coffee #76",
+        "category": "Home & Kitchen",
+        "in_stock": 137,
+        "status": "discontinued"
+    },
+    {
+        "id": 77,
+        "name": "Marketside Fresh Salad #77",
+        "category": "Clothing",
+        "in_stock": 476,
+        "status": "discontinued"
+    },
+    {
+        "id": 78,
+        "name": "Wonder Nation Kids' Shoes #78",
+        "category": "Sports",
+        "in_stock": 498,
+        "status": "inactive"
+    },
+    {
+        "id": 79,
+        "name": "Protege Suitcase #79",
+        "category": "Clothing",
+        "in_stock": 262,
+        "status": "discontinued"
+    },
+    {
+        "id": 80,
+        "name": "Athletic Works Joggers #80",
+        "category": "Beauty",
+        "in_stock": 180,
+        "status": "inactive"
+    },
+    {
+        "id": 81,
+        "name": "Mainstays Bed Sheet Set #81",
+        "category": "Beauty",
+        "in_stock": 166,
+        "status": "discontinued"
+    },
+    {
+        "id": 82,
+        "name": "Great Value Peanut Butter #82",
+        "category": "Books",
+        "in_stock": 365,
+        "status": "active"
+    },
+    {
+        "id": 83,
+        "name": "Equate Ibuprofen #83",
+        "category": "Home & Kitchen",
+        "in_stock": 392,
+        "status": "discontinued"
+    },
+    {
+        "id": 84,
+        "name": "Ozark Trail Water Bottle #84",
+        "category": "Beauty",
+        "in_stock": 288,
+        "status": "active"
+    },
+    {
+        "id": 85,
+        "name": "Parent's Choice Diapers #85",
+        "category": "Home & Kitchen",
+        "in_stock": 296,
+        "status": "discontinued"
+    },
+    {
+        "id": 86,
+        "name": "Hyper Tough Drill #86",
+        "category": "Beauty",
+        "in_stock": 158,
+        "status": "inactive"
+    },
+    {
+        "id": 87,
+        "name": "Better Homes & Gardens Candle #87",
+        "category": "Home & Kitchen",
+        "in_stock": 390,
+        "status": "inactive"
+    },
+    {
+        "id": 88,
+        "name": "Blackweb Wireless Mouse #88",
+        "category": "Toys",
+        "in_stock": 232,
+        "status": "active"
+    },
+    {
+        "id": 89,
+        "name": "Onn. HDMI Cable #89",
+        "category": "Electronics",
+        "in_stock": 338,
+        "status": "discontinued"
+    },
+    {
+        "id": 90,
+        "name": "George Men's T-Shirt #90",
+        "category": "Electronics",
+        "in_stock": 424,
+        "status": "inactive"
+    },
+    {
+        "id": 91,
+        "name": "Time and Tru Women's Jeans #91",
+        "category": "Books",
+        "in_stock": 384,
+        "status": "discontinued"
+    },
+    {
+        "id": 92,
+        "name": "No Boundaries Hoodie #92",
+        "category": "Clothing",
+        "in_stock": 184,
+        "status": "active"
+    },
+    {
+        "id": 93,
+        "name": "Your Zone Kids' Comforter #93",
+        "category": "Books",
+        "in_stock": 379,
+        "status": "discontinued"
+    },
+    {
+        "id": 94,
+        "name": "Hart 20V Battery #94",
+        "category": "Clothing",
+        "in_stock": 418,
+        "status": "active"
+    },
+    {
+        "id": 95,
+        "name": "Pen+Gear Notebook #95",
+        "category": "Home & Kitchen",
+        "in_stock": 74,
+        "status": "active"
+    },
+    {
+        "id": 96,
+        "name": "Sam's Choice Coffee #96",
+        "category": "Books",
+        "in_stock": 274,
+        "status": "active"
+    },
+    {
+        "id": 97,
+        "name": "Marketside Fresh Salad #97",
+        "category": "Electronics",
+        "in_stock": 323,
+        "status": "active"
+    },
+    {
+        "id": 98,
+        "name": "Wonder Nation Kids' Shoes #98",
+        "category": "Beauty",
+        "in_stock": 230,
+        "status": "discontinued"
+    },
+    {
+        "id": 99,
+        "name": "Protege Suitcase #99",
+        "category": "Sports",
+        "in_stock": 114,
+        "status": "discontinued"
+    },
+    {
+        "id": 100,
+        "name": "Athletic Works Joggers #100",
+        "category": "Clothing",
+        "in_stock": 177,
+        "status": "active"
+    }
+]
