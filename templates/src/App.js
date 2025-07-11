f96be7c5b0ac1a8dd82ec25b335e6209c8822ba8
Amritakanchan <amritakanchan00@gmail.com>
2025-07-12 02:23:56 +0530

    sidebar integrated

--- templates/src/App.js
+++ templates/src/App.js
@@ -3,35 +3,17 @@ import ProductTable from './components/Product_table';
 import './App.css';
 import AreaChart from './components/Line_graph';
 import Auth from './pages/Auth';
+import Sidebar from './components/Sidebar' 
 import PieChartOrder from './components/Piechart';
 import InventoryCapacityChart from './components/Inventory_capacity_chart';
 import Cards from './components/Cards';
 
-
 function App() {
   return (
     <div className="App">
-      {/* <div className='auth-page'>
-          <Auth/>
-        </div> */}
-         {/*leave this as it is for now */}
-
-      {/* <div className='demo-page'>
-         <div className='dummy-sidebar'> </div> dummy sidebar for refernceing
-         <div className='charts'>
-          <AreaChart />
-          <PieChartOrder />
-          <InventoryCapacityChart />
-          <ProductTable />
-         </div>
-          </div>   */}
-          {/* <Cards head="Pallet Incoming" value="120" color="#4CAF50" />
-          <Cards head="Pallet Outgoing" value="80" color="#2196F3" />
-          <Cards head="Low Stock Alert" value="5" color="#FF9800" /> */}
-     
-
+        <Sidebar />
     </div>
   );
-}
+};
 
 export default App;
---
+++ templates/src/components/Sidebar.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import '../styles/sidebar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTruckFast, faWarehouse } from '@fortawesome/free-solid-svg-icons';
+<span class="material-symbols-outlined">
+home
+</span>
+
+function Sidebar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    function hiddenBar() {
+      setIsOpen(true);
+    }
+
+    function closeBar() {
+      setIsOpen(false);
+    }
+
+    const customStyle = {
+      width: isOpen ? '200px' : '0px',
+      opacity: isOpen ? 1 : 0,
+      transition: 'width 0.4s cubic-bezier(0.4,0,0.2,1), opacity 0.3s',
+      overflow: 'hidden',
+      display: 'block',
+    };
+
+    return (
+      <div className='outerborder'>
+        <div className='sidebar'>
+          <div className='logo-con'>
+           <span onClick={hiddenBar} class="material-symbols-outlined logo">warehouse</span>
+          </div>
+          <div className='logo-object'>
+            <span onClick={hiddenBar} class="material-symbols-outlined object">home</span>
+          </div>
+          <div className='logo-object'>
+            <span onClick={hiddenBar} class="material-symbols-outlined object">local_shipping</span>
+          </div>
+          <div className='logo-object'>
+            <span onClick={hiddenBar} class="material-symbols-outlined object">monitoring</span>
+          </div>
+          <div className='logo-object'>
+            <span onClick={hiddenBar} class="material-symbols-outlined object">all_inbox</span>
+          </div>
+        </div>
+
+        <div id='Hidden' className='Hidden-bar' style={customStyle}>
+          <button style={{ float: 'right', margin: '8px', background: 'none', border: 'none', fontSize: '20px', cursor: 'pointer' }} onClick={closeBar}>&times;</button>
+          <div className='outline'>
+            <p>Inventory</p>
+          </div>
+          <div className='link1'>
+            <a href="">Suppliers</a> <br />
+          </div>
+          <div className='links'>
+            <a href="">Items List</a> <br />
+          </div>
+          <div className='links'>
+            <a href="">Profit Graph</a> <br />
+          </div>
+          <div className='links'>
+            <a href="">Home Page</a>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Sidebar;
+
