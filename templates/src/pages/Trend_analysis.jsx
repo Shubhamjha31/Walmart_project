a60f911bbb38a2b9d858f754f68d35cb9f9e28ad
Shubham Jha <jhashubham059@gmail.com>
2025-07-13 22:37:09 +0530

    some bug fixed

--- templates/src/pages/Trend_analysis.jsx
+++ templates/src/pages/Trend_analysis.jsx
@@ -6,8 +6,10 @@ export default function TrendAnalysis() {
 
     return(
         <div>
+            <h2 className="heading">Analysis</h2>
             <div className="forecast-graph">
                  <WarehouseSalesChart />
             </div>
         </div>
     )
+};
